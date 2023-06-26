9db05f361f4795f15a963af5e0262fe7637e101f
Alper Baskir <alper.baskir@rebtel.com>
2023-06-26 13:09:25 +0200

    preset file added

---
+++ tailwind-preset.js
@@ -0,0 +1,28 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            'sans' : ['FuturaPT-Book', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+            'futura-medium': ['FuturaPT-Medium']
+        },
+        colors: {
+            white: '#fff',
+            black: '#000000',
+            heading: '#1B1B1B',
+            brand: {
+              DEFAULT: '#E3232C',
+              secondary: '#E33D30',
+              dark: '#C61921'
+            },
+            gray: {
+              darker: '#333333',
+              dark: '#808080',
+              DEFAULT: '#C4C4C4',
+              light: '#DDDDDD',
+              lighter: '#F6F6F6',
+              lighterv2: '#D8D8D8',
+            },
+            success: '#25C72C'
+        }
+    },
+    plugins: [],
+  }
